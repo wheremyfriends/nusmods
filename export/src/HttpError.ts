2bccb2a80d8c1bad51c9046e34073cf8a92969f0
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2024-05-28 19:56:30 +0800

    refactor: move website to root (#26)
    * refactor: move website to root
    
    * Add gitignore
    
    * Remove ignored files
    
    * Update yarn.lock
    
    ---------
    
    Co-authored-by: WhyAre <laiyr52@gmail.com>

--- export/src/HttpError.ts
+++
@@ -1,9 +0,0 @@
-export class HttpError extends Error {
-  code: number;
-  originalError: Error | undefined;
-  constructor(code: number, message: string, originalError?: Error) {
-    super(message);
-    this.code = code;
-    this.originalError = originalError;
-  }
-}
