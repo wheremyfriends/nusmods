c3ddfcd25abfc30b33705fd9f39727f3730e9d19
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2024-05-29 16:06:22 +0800

    fix: exit edit mode when module is removed (#30)
    * fix: exit edit mode when module is removed
    
    * refactor: remove checks for double adding and removing

--- src/reducers/timetables.ts
+++ src/reducers/timetables.ts
@@ -317,6 +317,7 @@ function timetables(
       const { semester } = action.payload;
 
       return produce(state, (draft) => {
+        draft.editingType = null;
         draft.multiLessons[semester] = DEFAULT_SEM_TIMETABLE_MULTI_CONFIG;
         draft.lessons[semester] = DEFAULT_SEM_TIMETABLE_CONFIG;
         draft.colors[semester] = DEFAULT_SEM_COLOR_MAP;
@@ -325,23 +326,19 @@ function timetables(
     }
 
     case ADD_MODULE:
-      {
-        // Prevent double adding
-        if (isModuleInTimetable(action.payload.moduleCode, state.lessons, action.payload.semester))
-          return state;
-        else
-          return produceTimetableState(action.payload.semester, state, action);
-      }
+      return produceTimetableState(action.payload.semester, state, action);
     case REMOVE_MODULE:
       {
-        // Prevent double removing (likely not required)
-        if (!isModuleInTimetable(action.payload.moduleCode, state.lessons, action.payload.semester))
-          return state;
-        else {
-          const newState = produceTimetableState(action.payload.semester, state, action);
-          newState.multiLessons[action.payload.semester][action.payload.moduleCode] = {};
-          return newState;
-        }
+        const { semester, moduleCode } = action.payload;
+        return produce(state, (draft) => {
+          // Exit edit mode if editing module is removed
+          if (moduleCode == state.editingType?.moduleCode)
+            draft.editingType = null;
+          draft.multiLessons[semester][moduleCode] = {};
+          draft.lessons[semester] = semTimetable(draft.lessons[semester], action);
+          draft.colors[semester] = semColors(state.colors[semester], action);
+          draft.hidden[semester] = semHiddenModules(state.hidden[semester], action);
+        });
       }
     case SELECT_MODULE_COLOR:
     case CHANGE_LESSON:
