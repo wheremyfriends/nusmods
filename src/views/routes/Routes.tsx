d491262876a6bdeb029e2ba22e8397bf47535beb
kose <53458630+kokseen1@users.noreply.github.com>
2024-05-29 17:48:44 +0800

    feat: keep only roomID in url

--- src/views/routes/Routes.tsx
+++ src/views/routes/Routes.tsx
@@ -7,8 +7,7 @@ import NotFoundPage from 'views/errors/NotFoundPage';
 // IMPORTANT: Remember to update any route changes on the sitemap
 const Routes: React.FC = () => (
   <Switch>
-    <Redirect exact from="/" to="/timetable" />
-    <Route path="/timetable/:semester?/:roomID?" component={TimetableContainer} />
+    <Route path="/:roomID?" component={TimetableContainer} />
 
     {/* 404 page */}
     <Route component={NotFoundPage} />
