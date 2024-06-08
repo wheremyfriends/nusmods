8091c86a1a62eb28b1877cbe81c58f319b3b6f75
YR <laiyr52@gmail.com>
2024-06-08 16:05:02 +0800

    feat: add solver package (#39)
    * Add solver package
    
    * Add solver to package.json
    
    * Add example on usage
    
    * Add dist folder

---
+++ packages/solver/src/index.ts
@@ -0,0 +1,27 @@
+import { TimeSlot, Solver } from "./solver";
+import { preprocess, postprocess } from "./utils";
+
+export function getOptimisedTimetable(
+  timetables: any[],
+  index: number,
+  maxsols: number = -1
+) {
+  timetables = timetables.map((e) => {
+    return preprocess(e);
+  });
+
+  const solver = new Solver(timetables, index);
+  const solvedTimetable = solver.solve(maxsols);
+
+  const ret: TimeSlot[][] = [];
+  solvedTimetable.forEach((timetable) => {
+    let retTimetable: TimeSlot[] = [];
+    timetable.forEach((cls) => {
+      retTimetable = retTimetable.concat(postprocess(cls.timeslots));
+    });
+
+    ret.push(retTimetable);
+  });
+
+  return ret;
+}
