90fb8a17bc20bbadbf871e8ef74dccb21c378b5f
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2024-06-27 00:17:31 +0800

    feat: prettify roomid (#63)
    * feat: prettify roomid
    
    * Format code
    
    ---------
    
    Co-authored-by: kokseen1 <kokseen1@users.noreply.github.com>

--- src/views/routes/paths.ts
+++ src/views/routes/paths.ts
@@ -4,7 +4,11 @@ import { Venue } from "types/venues";
 import { SemTimetableConfig } from "types/timetables";
 import { serializeTimetable } from "utils/timetables";
 import config from "config";
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
+
+const alphabet =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const nanoid = customAlphabet(alphabet, 10);
 
 // IMPORTANT: Remember to update any route changes on the sitemap
 
