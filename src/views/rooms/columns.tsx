9ff608ddae4daa6411fdfd382ddc5e9fa20f2016
WhyAre <laiyr52@gmail.com>
2024-07-10 22:59:15 +0800

    Add UI components

---
+++ src/views/rooms/columns.tsx
@@ -0,0 +1,77 @@
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Checkbox } from "@/components/ui/checkbox";
+import { format } from "date-fns";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Room = {
+  roomID: string;
+  lastAccessed: Date;
+};
+
+export const columns: ColumnDef<Room>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "roomID",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          RoomID
+          <ArrowUpDown className="h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: (props: any) => (
+      <Link
+        className="text-[color:var(--body-color)]"
+        to={`/${props.getValue()}`}
+      >
+        {props.getValue()}
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "lastAccessed",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Accessed
+          <ArrowUpDown className="h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: (props: any) => (
+      <span>{format(props.getValue(), "dd MMM yyyy, hh:mm a")}</span>
+    ),
+  },
+];
