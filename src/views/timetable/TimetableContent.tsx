e550bd0f446c22876a4794725a7f2a26bcb1438f
WhyAre <laiyr52@gmail.com>
2024-07-14 13:24:35 +0800

    Fix apolloclient connection to get the auth to work

--- src/views/timetable/TimetableContent.tsx
+++ src/views/timetable/TimetableContent.tsx
@@ -82,6 +82,8 @@ import {
   ApolloProvider,
   gql,
   FetchResult,
+  HttpLink,
+  split,
 } from "@apollo/client";
 
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
@@ -93,6 +95,7 @@ import { Action } from "actions/constants";
 import { getOptimisedTimetable } from "solver";
 import venues from "data/venues";
 import ExamCalendar from "./ExamCalendar";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 export const CREATE_LESSON = gql`
   mutation CreateLesson(
@@ -173,9 +176,31 @@ const wsLink = new GraphQLWsLink(
   }),
 );
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 export const apolloClient = new ApolloClient({
-  uri: url,
-  link: wsLink,
+  // uri: url,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
