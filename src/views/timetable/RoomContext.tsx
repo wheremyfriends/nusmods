d8325f8e3281f93716e13879d0c670aef158cbb0
WhyAre <laiyr52@gmail.com>
2024-07-15 21:21:18 +0800

    Add readonly attribute to roomcontext
    No use as of now but maybe for the future

--- src/views/timetable/RoomContext.tsx
+++ src/views/timetable/RoomContext.tsx
@@ -3,7 +3,9 @@ import { createContext } from "react";
 export const RoomContext = createContext<{
   roomID: string | undefined;
   userID: number;
+  readOnly: boolean;
 }>({
   roomID: undefined,
   userID: -1,
+  readOnly: false,
 });
